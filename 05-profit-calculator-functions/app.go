3dc9c722e99877db3d9e2d7fec165a2836cb1499
Celso Alexandre <celsoalexandre@live.com>
2024-03-22 14:03:21 -0300

    05-profit-calculator-functions exercise

--- 05-profit-calculator-functions/app.go
+++ 05-profit-calculator-functions/app.go
@@ -3,34 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	var revenue float64
-	var expenses float64
-	var taxRate float64
+	revenue := readValueFromStdIo("Revenue: ")
+	expenses := readValueFromStdIo("Expenses: ")
+	taxRate := readValueFromStdIo("Tax Rate: ")
 
-	outputText("Revenue: ")
-	fmt.Scan(&revenue)
-	outputText("Expenses: ")
-	fmt.Scan(&expenses)
-	outputText("Tax Rate: ")
-	fmt.Scan(&taxRate)
-
-	var profit, profitAfterTax = calculateProfit(revenue, expenses, taxRate)
-	// var tax = profit * (taxRate / 100)
-	// var profitAfterTax = profit - tax
-	// var profitAfterTax = profit * (1 - (taxRate / 100))
+	profit, profitAfterTax, ratio := calculateProfit(revenue, expenses, taxRate)
 
 	fmt.Println()
-	fmt.Println("Profit:", profit)
-	fmt.Println("Profit after tax:", profitAfterTax)
+	fmt.Printf("Profit: %.2f\n", profit)
+	fmt.Printf("Profit after tax: %.2f\n", profitAfterTax)
+	fmt.Printf("Profit after tax ratio: %.2f\n", ratio)
 }
 
-func outputText(text string) {
-	fmt.Println(text)
+func readValueFromStdIo(label string) float64 {
+	var value float64
+	fmt.Print(label)
+	fmt.Scan(&value)
+	return value
 }
 
-func calculateProfit(revenue float64, expenses float64, taxRate float64) (profit float64, profitAfterTax float64) {
+func calculateProfit(revenue float64, expenses float64, taxRate float64) (profit float64, profitAfterTax float64, ratio float64) {
 	profit = revenue - expenses
 	profitAfterTax = profit * (1 - (taxRate / 100))
-	return profit, profitAfterTax
+	ratio = profit / profitAfterTax
+	return profit, profitAfterTax, ratio
 	// return
 }
