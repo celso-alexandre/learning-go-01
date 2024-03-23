526e3baf95feead01d9300b15fdb57d9b7a7cdc3
Celso Alexandre <celsoalexandre@live.com>
2024-03-23 20:17:57 -0300

    09-pointers

---
+++ 09-pointers/app.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	age := 30
+	agePointer := &age
+
+	fmt.Println("Age:", *agePointer)
+	adultYears := getAdultYears(agePointer)
+	fmt.Println("Adult years:", adultYears)
+}
+
+func getAdultYears(age *int) int {
+	return *age - 18
+}
---
+++ 09-pointers/go.mod
@@ -0,0 +1,3 @@
+module github.com/celso-alexandre/learning-go-01/09-pointers
+
+go 1.22.1
