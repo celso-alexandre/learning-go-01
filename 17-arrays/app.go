55c53a881936af4c3b6cc8cc56d1afde48151e47
Celso Alexandre <celsoalexandre@live.com>
2024-03-24 21:49:27 -0300

    17-arrays

--- 17-arrays/app.go
+++ 17-arrays/app.go
@@ -3,6 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	prices := [3]float64{1, 2.99, 3}
+	prices := [4]float64{1, 2.99, 3, 4.99}
 	fmt.Println(prices)
+
+	featuredPrices := prices[1:3] // from index 1 to index < 3 (1 to 2)
+	fmt.Println(featuredPrices)   // [2.99 3]
 }
