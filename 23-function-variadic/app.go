26ebeb1a3544a70ffa326270e048efe20bfc2bf9
Celso Alexandre <celsoalexandre@live.com>
2024-03-25 22:20:15 -0300

    22-function-variadic

--- 23-function-variadic/app.go
+++ 23-function-variadic/app.go
@@ -2,10 +2,10 @@ package main
 
 // Variadic function = a function that can accept a variable number of arguments.
 func main() {
+	println(sum())              // 0
 	println(sum(1, 2))          // 3
 	println(sum(1, 2, 3, 4))    // 10
 	println(sum(1, 2, 3, 4, 5)) // 15
-	println(sum())              // 0
 
 	numbers := []int{1, 2}
 	println(sum(numbers...)) // 3
