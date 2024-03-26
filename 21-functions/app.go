7e0f69cd2c6a633e0ba3459f9b3200b1890ccd65
Celso Alexandre <celsoalexandre@live.com>
2024-03-25 21:56:19 -0300

    21-functions

--- 21-functions/app.go
+++ 21-functions/app.go
@@ -2,26 +2,28 @@ package main
 
 import "fmt"
 
-type transformerFunc = func(int) int
+type transformFn = func(int) int
 
 func main() {
 	numbers := []int{1, 2, 3, 4, 5}
-	transformNumbers(&numbers, doubleNumber)
+	// transformNumbers(&numbers, doubleNumber)
+	transformNumbers(&numbers, func(number int) int { return number * 2 })
 	fmt.Println(numbers)
-	transformNumbers(&numbers, tripleNumber)
+	// transformNumbers(&numbers, tripleNumber)
+	transformNumbers(&numbers, func(number int) int { return number * 3 })
 	fmt.Println(numbers)
 }
 
-func transformNumbers(numbers *[]int, transformer transformerFunc) {
+func transformNumbers(numbers *[]int, transformer transformFn) {
 	for idx, number := range *numbers {
 		(*numbers)[idx] = transformer(number)
 	}
 }
 
-func doubleNumber(number int) int {
-	return number * 2
-}
+// func doubleNumber(number int) int {
+// 	return number * 2
+// }
 
-func tripleNumber(number int) int {
-	return number * 3
-}
+// func tripleNumber(number int) int {
+// 	return number * 3
+// }
